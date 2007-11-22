265920a0b7ede8ddc0661e7953b650931e3fe4d9
pasamio <pasamio@0ff2147c-982b-0410-b352-c9c47c792ec5>
2007-11-22 02:22:39 +0000

    Module completed, working on component

---
+++ personal_content/trunk/com_personalcontent/toolbar.personalcontent.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Selected Newsflash Toolbar -  Logic
+* @author Samuel Moffatt <pasamio@pasamio.id.au>
+* @copyright Copyright (C) 2006 Samuel Moffatt. All rights reserved
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+
+require_once( $mainframe->getPath( 'toolbar_html' ) );
+
+switch ($task) {
+	case 'new':
+		TOOLBAR_personalcontent::_NEW();
+		break;
+
+	default:
+		TOOLBAR_personalcontent::_DEFAULT();
+		break;
+}
+?>
