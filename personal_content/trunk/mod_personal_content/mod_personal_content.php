90fc9d9b7c4288798a81877ce62211d18ce8ddcc
Sam Moffatt <pasamio@users.noreply.github.com>
2007-11-22 06:32:38 +0000

    Fixed to include access permissions and publishing date

--- personal_content/trunk/mod_personal_content/mod_personal_content.php
+++ personal_content/trunk/mod_personal_content/mod_personal_content.php
@@ -21,7 +21,23 @@
  
 defined('_VALID_MOS') or die( 'Cant touch this!' );
 
-$database->setQuery('SELECT cid FROM #__personal_content WHERE uid = '. $my->id);
+global $my, $mosConfig_shownoauth, $mosConfig_offset, $acl;
+
+$now = date( 'Y-m-d H:i:s', time()+$mosConfig_offset*60*60 );
+$noauth = !$mainframe->getCfg( 'shownoauth' );
+$nullDate = $database->getNullDate();
+
+$query = 'SELECT cid ' 
+."\n FROM #__content AS a"
+."\n INNER JOIN #__personal_content AS p ON p.cid = a.id"
+."\n WHERE state != 0"
+."\n AND uid = ". $my->id
+. ( $noauth ? "\n AND a.access <= $my->gid" : '' )
+."\n AND (a.publish_up = '$nullDate' OR a.publish_up <= '$now' ) "
+."\n AND (a.publish_down = '$nullDate' OR a.publish_down >= '$now' )"
+."\n ORDER BY a.ordering";
+
+$database->setQuery($query);
 $result = intval($database->loadResult());
 $default = $params->get( 'default_cid', 0 );
 $anon = $params->get( 'anonymous_cid', 0 );
