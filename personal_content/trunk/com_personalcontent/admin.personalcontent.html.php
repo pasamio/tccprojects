3dd1ec49dc67319b889acfd65ee7927a7b778691
Sam Moffatt <pasamio@users.noreply.github.com>
2007-11-22 01:11:41 +0000

    Fun with component naming

---
+++ personal_content/trunk/com_personalcontent/admin.personalcontent.html.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Personal Content Manager - HTML Renderer
+ * 
+ * This file contains the HTML rendering classes 
+ * 
+ * PHP4/5
+ *  
+ * Created on Nov 22, 2007
+ * 
+ * @package Personal Content
+ * @subpackage Component
+ * @author Sam Moffatt <s.moffatt@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2007 Toowoomba City Council/Sam Moffatt 
+ * @version SVN: $Id:$
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/
+ */
+ 
+ 
+?>
