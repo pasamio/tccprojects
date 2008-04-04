0100eec44718a45d15667a750c75a03d5fcc1d8d
Sam Moffatt <pasamio@users.noreply.github.com>
2008-04-04 04:43:54 +0000

    Final version for 1.5

---
+++ azcontentlist/trunk/admin.azcontentlist.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Document Description
+ * 
+ * Document Long Description 
+ * 
+ * PHP4/5
+ *  
+ * Created on Apr 4, 2008
+ * 
+ * @package package_name
+ * @author Your Name <author@toowoomba.qld.gov.au>
+ * @author Toowoomba City Council Information Management Branch
+ * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
+ * @copyright 2008 Toowoomba City Council/Developer Name 
+ * @version SVN: $Id:$
+ * @see Project Documentation DM Number: #???????
+ * @see Gaza Documentation: http://gaza.toowoomba.qld.gov.au
+ * @see JoomlaCode Project: http://joomlacode.org/gf/project/
+ */
+ 
+JToolbarHelper::title('A-Z Content List');
+?><p>This component only has a front end part. You can link to this item to get an A-Z view of your content items.</p>
